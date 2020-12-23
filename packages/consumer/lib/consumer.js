dce89f205b9eaee8b375994516848565f1aad019
Amit Singh <amit1me@gmail.com>
2020-12-23 10:06:02 -0800

    tweak log

--- packages/consumer/lib/consumer.js
+++ packages/consumer/lib/consumer.js
@@ -5,6 +5,6 @@ module.exports = consumer;
 const printer = require("@silly-dev/printer");
 
 function consumer(message = "consumer-tag") {    
-    console.log("in consumer")
+    console.log("in consumer again")
     printer(message)
 }
