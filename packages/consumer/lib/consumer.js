e55991b1484a98fdd8253423f8150db2b3eb2ee8
Amit Singh <amit1me@gmail.com>
2020-12-25 21:20:10 -0800

    change text

--- packages/consumer/lib/consumer.js
+++ packages/consumer/lib/consumer.js
@@ -5,6 +5,6 @@ module.exports = consumer;
 const printer = require("@silly-dev/printer");
 
 function consumer(message = "consumer-tag") {    
-    console.log("changes from consumer side")
+    console.log(`consumer: got message ${message}`)
     printer(message)
 }
