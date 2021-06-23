6df17dd6f6de0a3ff0943bd3dfc5041c0df285f0
Amit Singh <amit1me@gmail.com>
2021-06-22 17:11:30 -0700

    change log

--- packages/consumer/lib/consumer.js
+++ packages/consumer/lib/consumer.js
@@ -5,6 +5,6 @@ module.exports = consumer;
 const printer = require("@silly-dev/printer");
 
 function consumer(message = "consumer-tag") {    
-    console.log(`consumer: message ${message}`)
+    console.log(`consumer: messag1 ${message}`)
     printer(message)
 }
